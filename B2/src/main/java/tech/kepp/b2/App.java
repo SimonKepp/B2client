f71cdeef4ba63d3a61817e9afe3f52bfb36fcd22
Simon Kepp Nielsen <SimonKepp@gmail.com>
2019-11-12 15:13:53 +0100

    added basic Maven and Eclipse support

---
+++ B2/src/main/java/tech/kepp/b2/App.java
@@ -0,0 +1,13 @@
+package tech.kepp.b2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
